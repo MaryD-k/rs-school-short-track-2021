81b24c60fcaa0a3cd647c3c857d3d41b4063a8eb
maryd-k <maryia.dyliuk@gmail.com>
2021-04-07 16:41:40 +0300

    feat: implement task 6

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,12 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const checkCondition = '([0-9A-F]{2}[-]){5}[0-9A-F]{2}';
+  if (n.search(checkCondition) === -1) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = isMAC48Address;
