bc90dfe5244eacbcef7d1ee64a293d89b39d83a5
maryd-k <maryia.dyliuk@gmail.com>
2021-04-07 22:05:09 +0300

    feat: implement task 7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,21 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const newNames = [...names];
+  for (let i = 0; i < names.length; i++) {
+    for (let j = 0, k = 0; j < names.length; j++) {
+      if (newNames[j] === newNames[i]) {
+        if (k !== 0) {
+          newNames[j] = `${names[i]}(${k})`;
+          k++;
+        } else {
+          k++;
+        }
+      }
+    }
+  }
+  return newNames;
 }
 
 module.exports = renameFiles;
