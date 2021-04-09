71f03703dd539f3858fe30277e0040da2e1c357f
maryd-k <maryia.dyliuk@gmail.com>
2021-04-09 15:29:41 +0300

    feat: implement task 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,68 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const newMatrix = [];
+  let j;
+  let i;
+  for (i = 0; i < matrix.length; i++) {
+    newMatrix[i] = [];
+    for (j = 0; j < matrix[i].length; j++) {
+      newMatrix[i][j] = 0;
+    }
+  }
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === true) {
+        newMatrix[i][j] = true;
+        if (i === 0) {
+          newMatrix[i + 1][j] += 1;
+        }
+        if (i > 0 && i !== matrix.length - 1) {
+          newMatrix[i - 1][j] += 1;
+          newMatrix[i + 1][j] += 1;
+        }
+        if (i === newMatrix.length - 1) {
+          newMatrix[i + 1][j] += 1;
+        }
+        if (j === 0) {
+          newMatrix[i][j + 1] += 1;
+        }
+        if (j > 0 && j !== matrix[i].length - 1) {
+          newMatrix[i][j - 1] += 1;
+          newMatrix[i][j + 1] += 1;
+        }
+        if (j === newMatrix[i].length - 1) {
+          newMatrix[i][j - 1] += 1;
+        }
+        if (j !== 0 && i !== 0) {
+          if (newMatrix[i - 1][j - 1] !== true) {
+            newMatrix[i - 1][j - 1] += 1;
+          }
+          if (newMatrix[i - 1][j + 1] !== true) {
+            newMatrix[i - 1][j + 1] += 1;
+          }
+          if (newMatrix[i + 1][j - 1] !== true) {
+            newMatrix[i + 1][j - 1] += 1;
+          }
+          if (newMatrix[i + 1][j + 1] !== true) {
+            newMatrix[i + 1][j + 1] += 1;
+          }
+        }
+      }
+      if (typeof newMatrix[i][j] !== 'number' && newMatrix[i][j] !== true) {
+        newMatrix[i][j] = 0;
+      }
+    }
+  }
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+      if (newMatrix[i][j] === true) {
+        newMatrix[i][j] = 1;
+      }
+    }
+  }
+  return newMatrix;
 }
 
 module.exports = minesweeper;
