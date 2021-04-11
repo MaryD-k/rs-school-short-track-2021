c6a37fe97ab82191ad6612f40ae1685b2a34c387
maryd-k <maryia.dyliuk@gmail.com>
2021-04-11 18:56:11 +0300

    feat: implement task 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,32 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const newObject = {};
+  const domainsMas = [];
+  let i;
+  let j;
+  for (i = 0; i < domains.length; i++) {
+    const k = domains[i].split('.').reverse();
+    domainsMas[i] = [];
+    for (j = 0; j < k.length; j++) {
+      if (j === 0) {
+        domainsMas[i][j] = `.${k[j]}`;
+      } else {
+        domainsMas[i][j] = `.${k.slice(0, j + 1).join('.')}`;
+      }
+    }
+  }
+  for (i = 0; i < domainsMas.length; i++) {
+    for (j = 0; j < domainsMas[i].length; j++) {
+      if (domainsMas[i][j] in newObject === true) {
+        newObject[domainsMas[i][j]] += 1;
+      } else {
+        newObject[domainsMas[i][j]] = 1;
+      }
+    }
+  }
+  return newObject;
 }
 
 module.exports = getDNSStats;
